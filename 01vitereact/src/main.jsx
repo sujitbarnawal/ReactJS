463529fac2dbc9cf8ab1755a809a9a1ceca12336
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-11 17:20:45 +0545

    injecting elements

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,12 +1,43 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 
+// function MyApp(){
+//     return(
+//          <>
+//         <div>Hello !</div>
+//         <p>React App</p>
+//         </>
+//     )
+// }
 
+// let reactElement={
+//     type:"a",
+//     props:{
+//         href:"www.google.com",
+//         target:"_blank"
+//     },
+//     children:"click me to visit Google"
+// }
+
+// let AnotherElement=(
+//     <a href="https://sujitbarnawal.com.np" target='_blank'>Visit Sujit</a>
+// )
+
+let username=" sujit"
+let reactElement=React.createElement(
+    "a",
+    {"href":"https://sujitbarnawal.com.np","target":"_blank"},
+    "Visit me",
+     username
+)
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
 
-    <App />
+    // <App />
+    // <MyApp/>
+    // AnotherElement
+    reactElement
 
 )
