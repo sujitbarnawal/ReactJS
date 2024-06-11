7458b9edc0bf00aed96a4ec8efd2224a242c1240
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-11 17:20:32 +0545

    injecting variables

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,10 @@
 import SayHello from "./sujit"
 
 function App() {
-  
+  const userName="Where are you?"
   return (
     <>
-    <h1>Sujit Barnawal</h1>
+    <h1>Sujit Barnawal {userName}</h1>
     <SayHello />
     </>
   )
