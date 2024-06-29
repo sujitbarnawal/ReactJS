d6e6aee42205aaf74038541ef2a2ddadae2ff176
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-06-29 17:49:47 +0545

    project completed

--- 07reactrouter/src/components/Footer/Footer.jsx
+++ 07reactrouter/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import {Link}  from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
