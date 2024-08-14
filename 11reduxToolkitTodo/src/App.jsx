ff617da0a4c200e14af6c59bc4e865c05b93ca23
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-08-14 21:16:23 +0545

    todo with redux tool kit

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
