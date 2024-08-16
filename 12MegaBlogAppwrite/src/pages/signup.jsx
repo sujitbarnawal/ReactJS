0afcc177090d643fb3e7c22f748d094a5daae969
sujitbarnawal <sujitbarnawal4545@gmail.com>
2024-08-16 12:46:54 +0545

    mega project

---
+++ 12MegaBlogAppwrite/src/pages/signup.jsx
@@ -0,0 +1,12 @@
+
+import { SignUp as SignupComponent } from '../components'
+
+function signup() {
+  return (
+    <div className='py-8'>
+        <SignupComponent />
+    </div>
+  )
+}
+
+export default signup
